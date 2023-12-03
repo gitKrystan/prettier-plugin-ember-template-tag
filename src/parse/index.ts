88b86d232c176c472e02f7071d8f13f88222cbfa
Krystan HuffMenne <kmenne+github@gmail.com>
2023-12-02 21:09:09 -0800

    Move normalizeWhitespace to separate file

--- src/parse/index.ts
+++ src/parse/index.ts
@@ -9,6 +9,7 @@ import type { Options } from '../options.js';
 import type { GlimmerTemplate, RawGlimmerTemplate } from '../types/glimmer';
 import { isDefaultTemplate } from '../types/glimmer';
 import { assert } from '../utils';
+import { normalizeWhitespace } from './whitespace';
 
 const typescript = babelParsers['babel-ts'] as Parser<Node | undefined>;
 const p = new Preprocessor();
@@ -116,58 +117,6 @@ function preprocess(code: string): Preprocessed {
   }
 
   return { results, code: output };
-}
-
-function replaceRange(
-  original: string,
-  range: { start: number; end: number },
-  substitute: string,
-): string {
-  return (
-    original.slice(0, range.start) + substitute + original.slice(range.end)
-  );
-}
-
-const STATIC_OPEN = 'static{`';
-const STATIC_CLOSE = '`}';
-const NEWLINE = '\n';
-
-function normalizeWhitespace(
-  templateNode: RawGlimmerTemplate,
-  originalCode: string,
-  currentCode: string,
-): string {
-  let prefix: string;
-  let suffix: string;
-
-  if (templateNode.type === 'class-member') {
-    prefix = STATIC_OPEN;
-    suffix = STATIC_CLOSE;
-  } else {
-    const nextWord = originalCode.slice(templateNode.range.end).match(/\S+/);
-    prefix = '{';
-    suffix = '}';
-    if (nextWord && nextWord[0] === 'as') {
-      prefix = '(' + prefix;
-      suffix = suffix + ')';
-    } else if (!nextWord || ![',', ')'].includes(nextWord[0][0] || '')) {
-      suffix += ';';
-    }
-  }
-
-  const lineBreakCount = [...templateNode.contents].reduce(
-    (sum, currentContents) => sum + (currentContents === NEWLINE ? 1 : 0),
-    0,
-  );
-  const totalLength = templateNode.range.end - templateNode.range.start;
-  const spaces = totalLength - prefix.length - suffix.length - lineBreakCount;
-  const content = ' '.repeat(spaces) + NEWLINE.repeat(lineBreakCount);
-
-  return replaceRange(
-    currentCode,
-    templateNode.range,
-    `${prefix}${content}${suffix}`,
-  );
 }
 
 export const parser: Parser<Node | undefined> = {
---
+++ src/parse/whitespace.ts
@@ -0,0 +1,53 @@
+import type { RawGlimmerTemplate } from '../types/glimmer';
+
+const STATIC_OPEN = 'static{`';
+const STATIC_CLOSE = '`}';
+const NEWLINE = '\n';
+
+function replaceRange(
+  original: string,
+  range: { start: number; end: number },
+  substitute: string,
+): string {
+  return (
+    original.slice(0, range.start) + substitute + original.slice(range.end)
+  );
+}
+
+export function normalizeWhitespace(
+  templateNode: RawGlimmerTemplate,
+  originalCode: string,
+  currentCode: string,
+): string {
+  let prefix: string;
+  let suffix: string;
+
+  if (templateNode.type === 'class-member') {
+    prefix = STATIC_OPEN;
+    suffix = STATIC_CLOSE;
+  } else {
+    const nextWord = originalCode.slice(templateNode.range.end).match(/\S+/);
+    prefix = '{';
+    suffix = '}';
+    if (nextWord && nextWord[0] === 'as') {
+      prefix = '(' + prefix;
+      suffix = suffix + ')';
+    } else if (!nextWord || ![',', ')'].includes(nextWord[0][0] || '')) {
+      suffix += ';';
+    }
+  }
+
+  const lineBreakCount = [...templateNode.contents].reduce(
+    (sum, currentContents) => sum + (currentContents === NEWLINE ? 1 : 0),
+    0,
+  );
+  const totalLength = templateNode.range.end - templateNode.range.start;
+  const spaces = totalLength - prefix.length - suffix.length - lineBreakCount;
+  const content = ' '.repeat(spaces) + NEWLINE.repeat(lineBreakCount);
+
+  return replaceRange(
+    currentCode,
+    templateNode.range,
+    `${prefix}${content}${suffix}`,
+  );
+}
