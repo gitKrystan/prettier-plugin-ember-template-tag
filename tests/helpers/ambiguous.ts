c9dc4d283ea84df3af11a2cda3565bbc6cb988be
Krystan HuffMenne <kmenne+github@gmail.com>
2023-12-02 20:39:22 -0800

    Snapshot parse errors in tests

--- tests/helpers/ambiguous.ts
+++ tests/helpers/ambiguous.ts
@@ -107,5 +107,6 @@ async function behavesLikeFormattedAmbiguousCase(
       throw error;
     }
     expect(isSyntaxError, 'Expected SyntaxError').toBeTruthy();
+    expect(error).toMatchSnapshot();
   }
 }
